53adb1d2ac04e5929d9f81715b7e92dc05bf03a7
andres085 <andresito.martinez1985@gmail.com>
2022-01-24 01:17:57 -0300

    Se modifico el archivo character.js dentro de api para adoptar el patron MVC, la logica se traslado al controlador con el nombre del mismo modelo

---
+++ src/app/controllers/character.controller.js
@@ -0,0 +1,106 @@
+const Sequelize = require('sequelize');
+const { Character } = require('../config/db.config');
+const Op = Sequelize.Op;
+
+
+exports.create = (req, res) => {
+    
+    Character.create(req.body)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message
+            })
+        })
+}
+
+exports.get = (req, res) => {
+    const name = req.query.name;
+    const age = req.query.age;
+
+    let condition = name ? { name: { [Op.like]: `%${name}%` } } : age ? { age: { [Op.like]: `%${age}%` } } : null;
+
+    Character.findAll({
+        where: condition,
+        attributes: {exclude: ['id', 'age', 'weight', 'story', 'createdAt', 'updatedAt']},
+        })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: 'Error retrieving all the characters'
+            })
+        })
+}
+
+exports.getOne = (req, res) => {
+
+    let id = req.params.id;
+
+    Character.findByPk(id)
+        .then(data => {
+            data ? res.send(data) : res.status(404).send({
+                message: `Cannot find character with id ${id}` 
+            })
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: `Error getting character with id ${id}`
+            })
+        })
+
+}
+
+exports.update = (req, res) => {
+
+    let id = req.params.id;
+
+    Character.update(req.body, {
+            where: {
+                id: id
+            }
+        })
+        .then(data => {
+            if (data == 1) {
+                res.send({
+                    message: `Character with id ${id} updated successfully`
+                })
+            } else {
+                res.send({
+                    message: `Cannot update character with id ${id}, not found or body it's empty`
+                })
+            }
+        })
+        .catch(err => {
+             res.status(500).send({
+                message: `Error updating character with id ${id}`
+            })
+        })
+}
+
+exports.delete = (req, res) => {
+
+    let id = req.params.id;
+
+    Character.destroy({
+        where: {id: id}
+    }).then(data => {
+            if (data == 1) {
+                res.send({
+                    message: `Character with id ${id} deleted successfully`
+                })
+            } else {
+                res.send({
+                    message: `Cannot delete character with id ${id}, not found`
+                })
+            }
+        })
+        .catch(err => {
+             res.status(500).send({
+                message: `Error deleting character with id ${id}`
+            })
+        })
+}
--- src/app/routes/api/characters.js
+++ src/app/routes/api/characters.js
@@ -1,62 +1,15 @@
 const router = require('express').Router();
-const Sequelize = require('sequelize');
-const Op = Sequelize.Op;
 
-const { Character } = require('../../config/db.config');
+const characters  = require('../../controllers/character.controller');
 
-router.get('/', async (req, res) => {
+router.post('/', characters.create);
 
-    const name = req.query.name;
-    const age = req.query.age;
+router.get('/', characters.get);
 
-    let condition = name ? { name: { [Op.like]: `%${name}%` } } : age ? { age: { [Op.like]: `%${age}%` } } : null;
-
-    const characters = await Character.findAll({
-        where: condition,
-        attributes: {exclude: ['id', 'age', 'weight', 'story', 'createdAt', 'updatedAt']},
-    });
-
-    res.json(characters);
-
-});
-
-router.get('/:characterId', async (req, res) => {
-    const character = await Character.findOne({
-        where: { id: req.params.characterId }
-    });
-
-    if (character) {
-        res.status(200).json({
-            data: character,
-            message: 'Character found successfully'
-        });
-    } else {
-        res.status(404).json({
-            message: 'Character not found'
-        });
-    }
-});
-
-router.post('/', async (req, res) => {
-    const character = await Character.create(req.body);
-
-    res.json(character);
-});
-
-router.put('/:characterId', async (req, res) => {
-    await Character.update(req.body, {
-        where: {
-            id: req.params.characterId
-        }
-    });
-    res.json({success: 'Character modified successfully'})
-});
-
-router.delete('/:characterId', async (req, res) => {
-    await Character.destroy({
-        where: {id: req.params.characterId}
-    });
-    res.json({ success: 'Character deleted successfully' });
-});
-
+router.get('/:id', characters.getOne);
+   
+router.put('/:id', characters.update);
+ 
+router.delete('/:id',characters.delete)
+  
 module.exports = router;
